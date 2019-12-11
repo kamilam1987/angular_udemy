69059d21e3394598e76a5a666255db3a568625d6
Kamila Michel <kmichel@gwy-kmichel-m.us.int.genesyslab.com>
2019-12-11 11:45:35 +0000

    custom pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import {CoursesService} from './courses.service';
+import { SummaryPipe} from './summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    SummaryPipe
     
   ],
   imports: [
